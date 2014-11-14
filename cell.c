6aef65e299d35fc3ed5b6d1b71867b369a2c62f7
John Toman <jtoman@cs.washington.edu>
2014-11-14 11:02:24 -0800

    now with a more interesting driver function

--- cell.c
+++ cell.c
@@ -17,7 +17,7 @@
   type unsafe_cell_unwrap(type)(unsafe_cell_t(type) *self) {			\
 	return self->value;													\
   }
-	
+
 #define cell_t(type) cell_##type##_t
 #define cell_new(type) Cell_##type##_new
 #define cell_get(type) Cell_##type##_get
@@ -67,7 +67,7 @@ typedef struct {
 option_t_impl(ref_t)
 
 ref_cell_t ref_cell_new(T value) {
-  ref_cell_t to_ret = { 0, unsafe_cell_new(T)(value) };
+  ref_cell_t to_ret = { cell_new(int)(0), unsafe_cell_new(T)(value) };
   return to_ret;
 }
 
@@ -104,7 +104,7 @@ option_t(ref_mut_t) ref_cell_try_mut_borrow(ref_cell_t *self) {
   }
 }
 
-ref_mut_t ref_cell_mut_borrow(ref_cell_t *self) {
+ref_mut_t ref_cell_borrow_mut(ref_cell_t *self) {
   option_t(ref_mut_t) _temp = ref_cell_try_mut_borrow(self);
   if(_temp.flag == NONE) {
 	__CPROVER_assume(1 == 0);
@@ -145,41 +145,209 @@ const T *ref_mut_deref(ref_mut_t *self) {
 short nondet_action();
 short nondet_target();
 
+typedef struct {
+  int borrowed_from;
+  int immutable;
+  int type;
+  int i;
+  int slot;
+} stack_info;
+
+/*
+ * actions:
+ * 0 - pop-stack
+ * 1 - new ref-cell
+ * 2 - rc_borrow
+ * 3 - rc_borrow_mut
+ * 4 - r_deref
+ * 5 - rm_deref
+ * 6 - rm_deref_mut
+ */
+
+/* 
+ * type codes:
+ * 0 - T*
+ * 1 - const T*
+ * 2 - RefMut<T>
+ * 3 - Ref<T>
+ * 4 - RefCell<T>
+ */
+
+int i_of_src(int slot, int type) {
+  if(type == 0) {
+	return slot;
+  } else if(type == 1) {
+	return slot + 2;
+  } else if(type == 2) {
+	return slot + 4;
+  } else if(type == 3) {
+	return slot + 7;
+  } else if(type == 4) {
+	return slot + 10;
+  }
+}
+
 int main(int argc, char **argv) {
   T val = { 2 };
   ref_cell_t m = ref_cell_new(val);
-  int m_flags[6] = { 0, 0, 0, 0, 0, 0 };
-  int im_flags[6] = { 0, 0, 0, 0, 0, 0 };
-  ref_mut_t mut_refs[6];
-  ref_t refs[6];
+  int live[11] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+  int users[11] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+  stack_info stack[10];
+  ref_cell_t r_cell[1];
+  ref_t refs[3];
+  ref_mut_t mut_refs[3];
+  T* t_ptr[2];
+  const T* const_t_ptr[2];
+  int stack_ptr = 0;
   int i;
   for(i = 0; i < 10; i++) {
 	int action = nondet_action();
-	__CPROVER_assume(action >= 0 && action < 4);
-	int target = nondet_target();
-	__CPROVER_assume(target >= 0 && target < 6);
-	if(action == 0) { // take mutable reference
-	  __CPROVER_assume(m_flags[target] == 0);
-	  mut_refs[target] = ref_cell_mut_borrow(&m);
-	  m_flags[target] = 1;
-	} else if(action == 1) { // drop mutable reference
-	  __CPROVER_assume(m_flags[target] == 1);
-	  m_flags[target] = 0;
-	  ref_mut_drop(&mut_refs[target]);
-	} else if(action == 2) { // take immutable reference
-	  __CPROVER_assume(im_flags[target] == 0);
-	  refs[target] = ref_cell_borrow(&m);
-	  im_flags[target] = 1;
-	} else if(action == 3) { // drop immutable reference
-	  __CPROVER_assume(im_flags[target] == 1);
-	  ref_drop(&refs[target]);
-	  im_flags[target] = 0;
+	__CPROVER_assume(action >= 0 && action < 7);
+	if(action > 0) {
+	  __CPROVER_assume(stack_ptr < 10);
 	}
-	for(int j = 0; j < 6; j++) {
-	  for(int k = 0; k < 6; k++) {
+	if(action == 0) {
+	  __CPROVER_assume(stack_ptr > 0);
+	}
+	if(action == 0) { // pop-stack
+	  stack_info *to_pop = &stack[stack_ptr-1];
+	  int i = stack[stack_ptr-1].i;
+	  // this was a borrow
+	  if(to_pop->borrowed_from != -1) {
+		if(to_pop->immutable) {
+		  users[to_pop->borrowed_from]--;
+		} else {
+		  users[to_pop->borrowed_from] = 0;
+		}
+	  }
+	  assert(users[to_pop->i] == 0);
+	  // variable is no longer live
+	  live[to_pop->i] = 0;
+	  if(to_pop->type == 0 || to_pop->type == 1) {
+		// no drop action
+	  } else if(to_pop->type == 2) {
+		ref_mut_drop(&mut_refs[to_pop->slot]);
+	  } else if(to_pop->type == 3) {
+		ref_drop(&refs[to_pop->slot]);
+	  } else if(to_pop->type == 4) {
+		// no drop action for RefCell
+	  }
+	  stack_ptr--;
+	} else if(action == 1) { // new ref-cell
+	  int slot = nondet_target();
+	  T value = { nondet_target() };
+	  __CPROVER_assume(slot >= 0 && slot < 1);
+	  int i = i_of_src(slot, 4);
+	  __CPROVER_assume(live[i] == 0);
+	  r_cell[slot] = ref_cell_new(value);
+	  live[i] = 1;
+	  stack[stack_ptr].type = 4;
+	  stack[stack_ptr].i = i;
+	  stack[stack_ptr].slot = slot;
+	  stack[stack_ptr].borrowed_from = -1;
+	  stack[stack_ptr].immutable = 0;
+	  stack_ptr++;
+	} else if(action == 2) { // rc_borrow
+	  int source_slot = nondet_target();
+	  int target_slot = nondet_target();
+	  __CPROVER_assume(source_slot >= 0 && source_slot < 1);
+	  __CPROVER_assume(target_slot >= 0 && target_slot < 3);
+	  int src_i = i_of_src(source_slot, 4);
+	  int target_i = i_of_src(target_slot, 3);
+	  __CPROVER_assume(live[src_i] == 1);
+	  __CPROVER_assume(live[target_i] == 0);
+	  // this is an "immutable" borrow
+	  __CPROVER_assume(users[src_i] != -1);
+	  refs[target_slot] = ref_cell_borrow(&r_cell[source_slot]);
+	  live[target_i] = 1;
+	  stack[stack_ptr].i = target_i;
+	  stack[stack_ptr].slot = target_slot;
+	  stack[stack_ptr].borrowed_from = src_i;
+	  stack[stack_ptr].type = 3;
+	  stack[stack_ptr].immutable = 1; // wink wink
+	  users[src_i]++;
+	  stack_ptr++;
+	} else if(action == 3) { // rc_borrow_mut
+	  int source_slot = nondet_target();
+	  int target_slot = nondet_target();
+	  __CPROVER_assume(source_slot >= 0 && source_slot < 1);
+	  __CPROVER_assume(target_slot >= 0 && target_slot < 3);
+	  int src_i = i_of_src(source_slot, 4);
+	  int target_i = i_of_src(target_slot, 2);
+	  __CPROVER_assume(live[src_i] == 1);
+	  __CPROVER_assume(live[target_i] == 0);
+	  __CPROVER_assume(users[src_i] != -1);
+	  mut_refs[target_slot] = ref_cell_borrow_mut(&r_cell[source_slot]);
+	  users[src_i]++;
+	  stack[stack_ptr].i = target_i;
+	  stack[stack_ptr].slot = target_slot;
+	  stack[stack_ptr].borrowed_from = src_i;
+	  stack[stack_ptr].immutable = 1;
+	  stack[stack_ptr].type = 2;
+	  stack_ptr++;
+	} else if(action == 4) { // ref deref
+	  int source_slot = nondet_target();
+	  int target_slot = nondet_target();
+	  __CPROVER_assume(source_slot >= 0 && source_slot < 3);
+	  __CPROVER_assume(target_slot >= 0 && target_slot < 2);
+	  int src_i = i_of_src(source_slot, 3);
+	  int target_i = i_of_src(target_slot, 1);
+	  __CPROVER_assume(live[src_i] == 1);
+	  __CPROVER_assume(live[target_i] == 0);
+	  __CPROVER_assume(users[src_i] != -1);
+	  const_t_ptr[target_slot] = ref_deref(&refs[source_slot]);
+	  live[target_i] = 1;
+	  users[src_i]++;
+	  stack[stack_ptr].i = target_i;
+	  stack[stack_ptr].slot = target_slot;
+	  stack[stack_ptr].borrowed_from = src_i;
+	  stack[stack_ptr].immutable = 1;
+	  stack[stack_ptr].type = 1;
+	  stack_ptr++;
+	} else if(action == 5) { // mut_ref deref
+	  int source_slot = nondet_target();
+	  int target_slot = nondet_target();
+	  __CPROVER_assume(source_slot >= 0 && source_slot < 3);
+	  __CPROVER_assume(target_slot >= 0 && target_slot < 2);
+	  int source_i = i_of_src(source_slot, 2);
+	  int target_i = i_of_src(target_slot, 1);
+	  __CPROVER_assume(live[source_i] == 1);
+	  __CPROVER_assume(live[target_i] == 0);
+	  __CPROVER_assume(users[source_i] != -1);
+	  const_t_ptr[target_slot] = ref_mut_deref(&mut_refs[source_slot]);
+	  live[target_i] = 1;
+	  users[source_i]++;
+	  stack[stack_ptr].i = target_i;
+	  stack[stack_ptr].slot = target_slot;
+	  stack[stack_ptr].borrowed_from = source_i;
+	  stack[stack_ptr].immutable = 1;
+	  stack[stack_ptr].type = 1;
+	  stack_ptr++;
+	} else if(action == 6) { // mut_ref deref_mut
+	  int source_slot = nondet_target();
+	  int target_slot = nondet_target();
+	  __CPROVER_assume(source_slot >= 0 && source_slot < 3);
+	  __CPROVER_assume(target_slot >= 0 && target_slot < 2);
+	  int source_i = i_of_src(source_slot, 2);
+	  int target_i = i_of_src(target_slot, 0);
+	  __CPROVER_assume(live[source_i] == 1);
+	  __CPROVER_assume(live[target_i] == 0);
+	  __CPROVER_assume(users[source_i] == 0);
+	  t_ptr[target_slot] = ref_mut_deref_mut(&mut_refs[source_slot]);
+	  live[target_i] = 1;
+	  users[source_i] = -1;
+	  stack[stack_ptr].i = target_i;
+	  stack[stack_ptr].slot = target_slot;
+	  stack[stack_ptr].borrowed_from = source_i;
+	  stack[stack_ptr].immutable = 0;
+	  stack[stack_ptr].type = 0;
+	  stack_ptr++;
+	}
+	for(int j = 0; j < 2; j++) {
+	  for(int k = 0; k < 2; k++) {
+		assert(live[i_of_src(j, 0)] == 0 || live[i_of_src(k, 1)] || t_ptr[i_of_src(j, 0)] != const_t_ptr[i_of_src(k, 1)]);
 		if(j == k) { continue; }
-		assert(m_flags[j] == 0 || m_flags[k] == 0 || mut_ref_mut_deref(&mut_refs[j]) != mut_ref_mut_deref(&mut_refs[k]));
-		assert(m_flags[j] == 0 || im_flags[k] == 0 || mut_ref_mut_deref(&mut_refs[j]) != ref_deref(&refs[k]));
+		assert(live[i_of_src(j, 0)] == 0 || live[i_of_src(k, 0)] || t_ptr[i_of_src(j, 0)] != t_ptr[i_of_src(k, 0)]);
 	  }
 	}
   }
