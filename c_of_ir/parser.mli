f32ed11eb1fb837af1a9c1cfd3334644899c928f
John Toman <jtoman@cs.washington.edu>
2014-11-19 09:08:43 -0800

    adds parser

---
+++ c_of_ir/parser.mli
@@ -0,0 +1,13 @@
+val parse_channel : in_channel -> Ir.module_expr list
+val parse_string : string -> Ir.module_expr list
+(*
+val consume_to_end :
+           'a list ->
+           ('a list -> ('b -> 'a list -> 'c) -> 'c) -> ('b list -> 'c) -> 'c
+val parse_n : (string list -> ('a -> string list -> 'b) -> 'b) ->
+			  string list -> ('a list -> string list -> 'b) -> 'b
+val parse_module : string list ->
+				   (Ir.module_expr -> string list -> Ir.module_expr list) ->
+				   Ir.module_expr list
+val do_test : unit -> Ir.module_expr list
+ *)
