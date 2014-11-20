a9d96a33239d2df50f295c59cfdcd2d183a67dc0
John Toman <jtoman@cs.washington.edu>
2014-11-19 19:54:05 -0800

    Populates env from parse-tree

--- c_of_ir/env.ml
+++ c_of_ir/env.ml
@@ -1,3 +1,19 @@
 type adt_env_t = (string,Ir.type_expr) Hashtbl.t
 let (adt_env : adt_env_t) = Hashtbl.create 10;;
 let (fn_env : (string,Ir.fn_def) Hashtbl.t) = Hashtbl.create 10;;
+
+let rec set_env = function 
+  | [] -> ()
+  | ((`Enum_def {
+		Ir.enum_name = adt_name;
+		_
+	}) as adt)::t
+  | ((`Struct_def {
+		Ir.struct_name = adt_name;
+		_
+	  }) as adt)::t -> 
+	 Hashtbl.add adt_env adt_name adt;
+	 set_env t
+  | (`Fn f)::t -> 
+	 Hashtbl.add fn_env f.Ir.fn_name f;
+	 set_env t
--- c_of_ir/env.mli
+++ c_of_ir/env.mli
@@ -1,5 +1,7 @@
 type adt_env_t = (string,Ir.type_expr) Hashtbl.t
 val adt_env : adt_env_t
 val fn_env : (string,Ir.fn_def) Hashtbl.t
+val set_env : Ir.module_expr list -> unit
+
 
 
