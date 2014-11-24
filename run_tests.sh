6b16b49d2cb68212cd38b215274899298de752bd
John Toman <jtoman@cs.washington.edu>
2014-11-24 11:49:40 -0800

    adds test script

---
+++ run_tests.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+PROJECT_DIR=$(dirname $0);
+
+make -C $PROJECT_DIR all > /dev/null
+
+# find the rust libraries
+RUSTC_LOC=$(whereis rustc | awk '{print $2}')
+if [ "$RUSTC_LOC" = "" ]; then
+	echo "Could not find rustc";
+	exit -1
+fi
+
+RUST_BIN=$(dirname $RUSTC_LOC)
+RUST_LIB=$(find $RUST_BIN/../lib -mindepth 2 -maxdepth 2 -type d)/lib
+
+RBMC_FLAGS="-A warnings -L $RUST_LIB"
+
+for i in $(find $PROJECT_DIR/tests -type f -name '*.rs' | sort); do
+	OUT_FILE=$(mktemp);
+	$PROJECT_DIR/rbmc $RBMC_FLAGS $i 2> /dev/null | $PROJECT_DIR/crust/crust.byte - 2> /dev/null > $OUT_FILE;
+	if [ \! $? -eq 0 ]; then
+		echo $(basename $i) FAILED;
+		rm $OUT_FILE;
+		continue;
+	fi
+	gcc -Wall -x c -c -o /dev/null $OUT_FILE;
+	if [ $? -eq 0 ]; then
+		echo $(basename $i) PASS
+	else
+		echo $(basename $i) FAILED
+	fi
+	rm $OUT_FILE;
+done
