22c078e33ce72a660dd50328aea1bb4d2ba00843
John Toman <jtoman@cs.washington.edu>
2014-12-01 20:55:05 -0800

    tests for tuples, refs, and borrow analysis

---
+++ tests/ref.rs
@@ -0,0 +1,35 @@
+#![crate_type = "lib"]
+
+struct S {
+    x: int
+}
+
+impl S {
+    fn do_thing(x: &int) -> int{
+        *x + 1
+    }
+    
+}
+
+fn whatever(y: int, x : (int,(uint,&int))) -> &int {
+    match x {
+        (_,(_,p)) => p
+    }
+}
+
+struct Cont<T> {
+    contents: T
+}
+
+impl<T> Cont<T> {
+    fn get_mut_ref<'a>(&'a mut self) -> &'a mut T {
+        &mut self.contents
+    }
+    fn get_ref(&self) -> &T {
+        &self.contents
+    }
+}
+
+fn crust_init () -> (S,Cont<int>) {
+    (S { x : 4 },Cont { contents: 4 })
+}
