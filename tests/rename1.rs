974c3edbb3f391b1ee7c04b28af094ccfa0c85f4
Stuart Pernsteiner <spernste@cs.washington.edu>
2015-01-15 16:12:34 -0800

    add tests/rename1

---
+++ tests/rename1.rs
@@ -0,0 +1,36 @@
+#![crate_type = "lib"]
+#![no_std]
+extern crate core;
+
+fn f() -> i32 {
+    let x = 1_i32;
+    let x = 2_i32;
+    let x = 3_i32;
+    x
+}
+
+fn g() -> i32 {
+    {
+        let x = 1_i32;
+        x;
+    }
+    {
+        let x = 1_i32;
+        x
+    }
+}
+
+fn h() -> i32 {
+    let x = 1_i32;
+    {
+        let x = 2_i32;
+    }
+    x
+}
+
+fn i(x: i32) -> i32 {
+    let x = 1_i32;
+    x
+}
+
+fn crust_init() -> (uint, ) { (0, ) }
