e1cf33c61e8a9cef30606bd98bd8a88217711fc0
John Toman <jtoman@cs.washington.edu>
2015-01-19 12:24:40 -0800

    removes function filtering from crust
    as mentioned earlier, this is now handled in rbmc

--- src/crust/crust.ml
+++ src/crust/crust.ml
@@ -7,39 +7,11 @@ let get_sets () =
 						   ) Env.fn_env Analysis.FISet.empty in
   (t_set,f_set)
 
-let filter_file = ref None;;
-
-let filter_ast ast = 
-  let read_filter_rules f_name = 
-    let f_in = open_in f_name in
-    let rec read_loop accum = 
-      try
-        let l = input_line f_in in
-        if l = "" then
-          accum
-        else
-          read_loop @@ SSet.add l accum
-      with End_of_file -> close_in f_in; accum
-    in
-    read_loop SSet.empty
-  in
-  match !filter_file with
-  | None -> ast
-  | Some f_file ->
-    let filter_fn = read_filter_rules f_file in
-    List.filter (function
-        | `Fn { Ir.fn_name = n; _ } ->
-          not (SSet.mem n filter_fn)
-        | _ -> true
-      ) ast
-;;
-
 let do_it f = 
   Printexc.record_backtrace true;
   let input_file = ref "" in
   let arg_spec = [
     ("-gcc", Arg.Set Env.gcc_mode, "Turn on gcc mode");
-    ("-filter", Arg.String (fun f_name -> filter_file := Some f_name), "Filter function names");
     ("-infer-filter", Arg.String (fun f_name -> Env.init_inference_filter f_name), "Read a list of types for which to filter public function inference from file f (one per line)");
     ("-", Arg.Unit (fun () -> input_file := "-"), "Read from stdin");
   ] in
@@ -51,11 +23,10 @@ let do_it f =
 	  (open_in !input_file),true
   in
   let ast =
-  (try
-	Parser.parse_channel ~close:close input 
-  with Parser.Parse_failure (f_name,tokens) ->
-	failwith @@ "Parse failure in " ^ f_name ^ " on input " ^ (String.concat " " tokens)
-  ) |> filter_ast
+    try
+	  Parser.parse_channel ~close:close input 
+    with Parser.Parse_failure (f_name,tokens) ->
+	  failwith @@ "Parse failure in " ^ f_name ^ " on input " ^ (String.concat " " tokens)
   in
   Env.set_env ast;
   let w_state = Analysis.run_analysis () in
