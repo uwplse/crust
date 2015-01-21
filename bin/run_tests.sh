aca05885531ea432082b7739c29aca60b79bdfd8
Stuart Pernsteiner <spernste@cs.washington.edu>
2015-01-21 11:06:10 -0800

    make run_tests.sh give pretty output

--- bin/run_tests.sh
+++ bin/run_tests.sh
@@ -7,21 +7,24 @@ make -C $PROJECT_DIR all > /dev/null
 
 RBMC_FLAGS="-A warnings -L $PROJECT_DIR/lib"
 
+FAILED=$'\x1b[20G\x1b[31mFAILED\x1b[0m'
+PASSED=$'\x1b[20G\x1b[32mPASSED\x1b[0m'
+
 for i in $(find $PROJECT_DIR/tests -type f -name '*.rs' | sort); do
 	OUT_FILE=$(mktemp);
 	$BIN_DIR/rbmc $RBMC_FLAGS $i 2> /dev/null | \
         $BIN_DIR/Preprocess 2> /dev/null | \
 		$BIN_DIR/crust.native -optional-init -gcc - 2> /dev/null > $OUT_FILE;
 	if [ \! $? -eq 0 ]; then
-		echo $(basename $i) FAILED;
+		echo $(basename $i) $FAILED;
 		rm $OUT_FILE;
 		continue;
 	fi
 	gcc -std=c99 -x c -I $PROJECT_DIR/src -c -o /dev/null $OUT_FILE;
 	if [ $? -eq 0 ]; then
-		echo $(basename $i) PASS
+		echo $(basename $i) $PASSED
 	else
-		echo $(basename $i) FAILED
+		echo $(basename $i) $FAILED
 	fi
 	rm $OUT_FILE;
 done
