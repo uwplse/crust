7bc21fdcf7c770a18f213df7109db13465a41b65
John Toman <jtoman@cs.washington.edu>
2015-01-26 18:10:44 -0800

    fixes the location of rbmc

--- bin/run_one.sh
+++ bin/run_one.sh
@@ -5,7 +5,7 @@ THIS_DIR=$(cd $(dirname $0) && pwd);
 
 RBMC_FLAGS="-A warnings -L $THIS_DIR/../lib"
 
-$THIS_DIR/../src/rbmc $RBMC_FLAGS "$1" >out.ir
+$THIS_DIR/rbmc $RBMC_FLAGS "$1" >out.ir
 <out.ir $THIS_DIR/../src/Preprocess >out2.ir
 <out2.ir $THIS_DIR/../src/crust/crust.native -optional-init -gcc - >out.c
 gcc -c out.c -std=c99 -I $THIS_DIR/../src
