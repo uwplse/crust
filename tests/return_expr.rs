2d59297b610e5e2fcdcab893e6d0d0ca57c15758
Stuart Pernsteiner <spernste@cs.washington.edu>
2015-02-10 17:23:56 -0800

    add tests/return_expr

---
+++ tests/return_expr.rs
@@ -0,0 +1,19 @@
+#![crate_type = "lib"]
+#![no_std]
+
+extern crate core;
+
+fn test(x: bool) {
+    if x {
+        return
+    }
+}
+
+fn test2() {
+    let x = return;
+    x
+}
+
+fn test3() {
+    return
+}
