24a21af4aa884e821e6aa61fb726b71e36201cb1
John Toman <jtoman@cs.washington.edu>
2015-02-23 13:09:38 -0800

    removes borrow/move analysis
    we don't use this anymore because this is done in the rust
    compiler for us

--- src/crust/analysis.mli
+++ src/crust/analysis.mli
@@ -1,23 +1,6 @@
 module FISet : Set.S with type elt = string * (Types.mono_type list)
 module TISet : Set.S with type elt = TypeUtil.type_inst
 
-
-type borrow_nested = [
-  | `Tuple of int  * borrow_nested
-  | `MutableBorrow of int
-  | `ImmutableBorrow of int
-]
-
-type borrow_info = [
-  | borrow_nested
-  | `NoBorrow
-]
-
-type move_info = [
-  | `Move_val of int
-  | `Move_tuple of int * (move_info list)
-]
-
 type walk_state = {
   type_inst : TISet.t;
   fn_inst : FISet.t;
@@ -29,9 +12,6 @@ type walk_state = {
 exception ResolutionFailed of string
 
 val run_analysis : unit -> walk_state
-val run_test_analysis : unit -> walk_state
-val borrow_analysis : Ir.fn_def -> borrow_info
 val resolve_abstract_fn : string -> Types.mono_type list -> (Types.mono_type list * string)
 val init_fn_filter : string -> unit
 val set_fn_filter : string -> unit
-val move_analysis : Types.mono_type list -> move_info list
