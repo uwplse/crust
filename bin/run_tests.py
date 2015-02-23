4d38b7e193b0f36f832763756bce0835d1cc9ed3
John Toman <jtoman@cs.washington.edu>
2015-02-23 13:14:26 -0800

    adds z3 test running options
    progress output, other niceties

--- bin/run_tests.py
+++ bin/run_tests.py
@@ -4,22 +4,37 @@ import subprocess
 import argparse
 import os.path
 import sys
+import logging
+import time
+
+#logger = multiprocessing.log_to_stderr()
+#logger.setLevel(logging.DEBUG)
 
 cbmc_binary = None
 unwind_bound = None
 test_file = None
 include_dir = None
+use_z3 = False
 
 def run_test_case(test_case_name):
     dev_null = open("/dev/null", "w")
-    ret_code = subprocess.call([ cbmc_binary, "--pointer-check", "--bounds-check", "-I", include_dir, "--unwind", unwind_bound, "--function", test_case_name, test_file], stdout = dev_null, stderr = subprocess.STDOUT)
+    ret_code = subprocess.call([ cbmc_binary, "--pointer-check", "--bounds-check", 
+                                 "-I", include_dir, "--unwind", unwind_bound, 
+                                 "--z3" if use_z3 else "", 
+                                 "--function", test_case_name, 
+                                 test_file], 
+                               stdout = dev_null, stderr = subprocess.STDOUT)
     return ret_code
 
+
 def worker_thread(control_pipe, notify_queue, work_queue):
+    _worker_thread(control_pipe, notify_queue, work_queue)
+
+def _worker_thread(control_pipe, notify_queue, work_queue):
     while True:
         if control_pipe.poll():
-            control_pipe.get()
-            notify_queue.put(('quit', multiprocessing.current_process().pid))
+            control_pipe.recv()
+            notify_queue.put(('quit', multiprocessing.current_process().pid, multiprocessing.current_process().name))
             return True
         work = None
         try:
@@ -27,20 +42,20 @@ def worker_thread(control_pipe, notify_queue, work_queue):
         except Queue.Empty:
             pass
         except Exception as e:
-            notify_queue.put(('error', multiprocessing.current_process().pid,e))
+            notify_queue.put(('error', multiprocessing.current_process().pid,e,multiprocessing.current_process().name))
             return False
         if work is None:
-            notify_queue.put(("done", multiprocessing.current_process().pid))
+            notify_queue.put(("done", multiprocessing.current_process().pid,multiprocessing.current_process().name))
             return False
         result = None
         try:
-            result = run_test_case(work)
+            result = run_test_case(*work)
         except Exception as e:
-            notify_queue.put(("error", multiprocessing.current_process().pid,e))
+            notify_queue.put(("error", multiprocessing.current_process().pid,e,multiprocessing.current_process().name))
             return False
 
         if result != 0:
-            notify_queue.put(('fail', multiprocessing.current_process().pid,work))
+            notify_queue.put(('fail', multiprocessing.current_process().pid,work,multiprocessing.current_process().name))
             return False
 
 def run_master():
@@ -51,6 +66,7 @@ def run_master():
     parser.add_argument("-I", dest="include_dir", action="store", default=os.path.join(os.path.join(this_dir, ".."), "src"))
     parser.add_argument("--unwind", action="store", type = int)
     parser.add_argument("input", action="store")
+    parser.add_argument("--z3", action="store_true")
     parser.add_argument("test_names", action="store", nargs = "?")
 
     opts = parser.parse_args()
@@ -58,11 +74,13 @@ def run_master():
     global unwind_bound
     global test_file
     global include_dir
+    global use_z3
 
     cbmc_binary = opts.cbmc
     unwind_bound = str(opts.unwind)
     test_file = opts.input
     include_dir = opts.include_dir
+    use_z3 = opts.use_z3
     
     test_names = []
     def slurp_tests(f):
@@ -82,8 +100,10 @@ def run_master():
     work_queue = multiprocessing.Queue(len(test_names))
     notify_queue = multiprocessing.Queue(n_workers * 2)
     p_map = {}
-    for t  in test_names:
+    for t in test_names:
         work_queue.put(t, False)
+    num_tests = len(test_names)
+    test_names = None
     for i in range(0, n_workers):
         parent_conn, child_conn = multiprocessing.Pipe(True)
         worker_p = multiprocessing.Process(target = worker_thread, args = (child_conn,notify_queue, work_queue))
@@ -97,14 +117,27 @@ def run_master():
                 proc.join()
                 del p_map[pid]
                 continue
-            print "cleaning up: " + str(pid)
+#            print "cleaning up: " + str(pid)
             try:
                 control_conn.send(True)
             except IOError:
                 continue
 
+    start_time = time.time()
+    def print_progress():
+        curr_time = time.time()
+        msg = "Elapsed: " + str(curr_time - start_time)
+        total_tests = str(num_tests)
+        processed_tests = num_tests - work_queue.qsize()
+        print msg
+        print "Processed approx " + str(processed_tests) + " of " + total_tests
+
     while len(p_map):
-        msg = notify_queue.get()
+        try:
+            msg = notify_queue.get(True, 15)
+        except Queue.Empty:
+            print_progress()
+            continue
         if msg[1] not in p_map:
             continue
         (proc,_) = p_map[msg[1]]
@@ -118,8 +151,6 @@ def run_master():
             print "Test case " + str(msg[2]) + " failed!"
         cleanup_procs()
 
-    work_queue.close()
-    notify_queue.close()
 
 if __name__ == '__main__':
     run_master()
