45baa0f1329fb82d270a2f012a901be33229e687
Stuart Pernsteiner <spernste@cs.washington.edu>
2015-02-27 10:51:58 -0800

    add tests using original panic!() macro

---
+++ tests/panic1.rs
@@ -0,0 +1,8 @@
+#![crate_type = "lib"]
+#![no_std]
+#[macro_use] extern crate core;
+
+
+fn die() {
+    panic!("oh no");
+}
---
+++ tests/panic2.rs
@@ -0,0 +1,12 @@
+#![crate_type = "lib"]
+#![no_std]
+#[macro_use] extern crate core;
+
+mod std {
+    pub use core::fmt;
+}
+
+
+fn die() {
+    panic!("oh {}", "no");
+}
