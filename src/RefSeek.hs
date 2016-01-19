932f06f0c194e3b8b905b89060173c843d511342
Stuart Pernsteiner <spernste@cs.washington.edu>
2016-01-19 15:28:04 -0800

    use `ref` patterns when seeking refs

--- src/RefSeek.hs
+++ src/RefSeek.hs
@@ -12,6 +12,7 @@ import qualified Data.Map as M
 import Data.Maybe
 import qualified Data.Set as S
 
+import Builder
 import Index
 import Parser
 import Pprint (ppTy, runPp)
@@ -19,9 +20,9 @@ import Unify
 
 
 withCollectedRefs :: Index -> ([Expr] -> Expr) -> Expr -> Expr
-withCollectedRefs ix mkBody input = evalState (go [] [input]) 0
+withCollectedRefs ix mkBody input = evalState (go [] [addrOfExpr input]) 0
   where
-    go acc (e@(Expr ty _) : es) = case ty of
+    go acc (e@(Expr (TRef _ _ ty) _) : es) = case ty of
         TAdt name _ tas -> case fromMaybe (error $ "no type " ++ show name) $
                                 M.lookup name $ i_types ix of
             -- TODO: may not be able to do this if the adt has a dtor
@@ -33,13 +34,14 @@ withCollectedRefs ix mkBody input = evalState (go [] [input]) 0
             TEnum (EnumDef _ _ tps variants _) -> do
                 let variants' = map (onVariantTy $ subst ([], tps) ([], tas)) variants
                 arms <- zipWithM (mkMatchArm ty $ \es' -> go acc (es' ++ es)) [0..] variants'
-                return $ Expr TBottom $ EMatch e arms
+                return $ Expr TBottom $ EMatch (derefExpr e) arms
         TTuple tys -> do
             stmts <- zipWithM (mkTupleLet e) [0..] tys
             expr <- go acc (map letToExpr stmts ++ es)
             return $ Expr TBottom $ EBlock stmts expr
         TRef _ _ _ -> go (e : acc) es
         _ -> go acc es
+    go acc (e : es) = go acc es
     go acc [] = return $ mkBody acc
 
     onFieldTy f (FieldDef name ty) = FieldDef name (f ty)
@@ -47,18 +49,18 @@ withCollectedRefs ix mkBody input = evalState (go [] [input]) 0
 
     mkStructLet base (FieldDef name ty) = do
         varName <- fresh name
-        return $ SLet (Pattern ty $ PVar varName) (Just $ Expr ty $ EField base name)
+        return $ SLet (Pattern (refTy ty) $ PRefVar varName) (Just $ Expr ty $ EField base name)
 
     mkTupleLet base idx ty = do
         varName <- fresh $ "tuple" ++ show idx
-        return $ SLet (Pattern ty $ PVar varName) (Just $ Expr ty $ EField base $ "field" ++ show idx)
+        return $ SLet (Pattern (refTy ty) $ PRefVar varName) (Just $ Expr ty $ EField base $ "field" ++ show idx)
 
-    letToExpr (SLet (Pattern ty (PVar name)) _) = Expr ty $ EVar name
+    letToExpr (SLet (Pattern (TRef _ _ ty) (PRefVar name)) _) = Expr ty $ EVar name
 
     mkMatchArm ty mkBody idx (VariantDef name tys) = do
         varNames <- replicateM (length tys) (fresh name)
-        let pats = zipWith (\v t -> Pattern t $ PVar v) varNames tys
-        expr <- mkBody $ zipWith (\v t -> Expr t $ EVar v) varNames tys
+        let pats = zipWith (\v t -> Pattern (refTy t) $ PRefVar v) varNames tys
+        expr <- mkBody $ zipWith (\v t -> Expr (refTy t) $ EVar v) varNames tys
         return $ MatchArm (Pattern ty $ PEnum name idx pats) expr
 
     fresh base = do
@@ -67,6 +69,10 @@ withCollectedRefs ix mkBody input = evalState (go [] [input]) 0
         return $ "vr_" ++ clean base ++ "_" ++ show idx
 
     clean name = map (\c -> case c of '$' -> '_'; _ -> c) name
+
+    refTy t = TRef "_" MImm t
+    derefExpr e = mkE ix $ deref (return e)
+    addrOfExpr e = mkE ix $ addrOf' MImm (return e)
 
 hasRef :: Index -> Ty -> Bool
 hasRef ix ty = case ty of
